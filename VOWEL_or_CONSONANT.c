55487025fc603ab3906b829fec529453e08691d0
kanchuharish24 <119840058+kanchuharish24@users.noreply.github.com>
2022-12-24 19:15:09 +0530

    #include<stdio.h> int main() {     char z;     scanf("%c",&z);     if(z=='a'||z=='e'||z=='i'||z=='o'||z=='u'||z=='A'||z=='E'||z=='I'||z=='O'||z=='U')     printf("VOWEL");     else printf("CONSONANT");
    }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char z;
+    scanf("%c",&z);
+    if(z=='a'||z=='e'||z=='i'||z=='o'||z=='u'||z=='A'||z=='E'||z=='I'||z=='O'||z=='U')
+    printf("VOWEL");
+    else printf("CONSONANT");
+    
+}
