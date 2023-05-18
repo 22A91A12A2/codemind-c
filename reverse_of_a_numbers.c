7e1e94b24740315e093efad1af001268fac410b1
Kanchu harish <119840058+kanchuharish24@users.noreply.github.com>
2023-05-18 21:16:58 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,i,r,b,req=0;     scanf("%d",&a);     for(i=a;a!=0;a=a/10)     {         r=a%10;         req=req*10+r;     }     printf("%d",req); }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,i,r,b,req=0;
+    scanf("%d",&a);
+    for(i=a;a!=0;a=a/10)
+    {
+        r=a%10;
+        req=req*10+r;
+    }
+    printf("%d",req);
+}
