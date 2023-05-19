9fc3fea79df9243a27d9f2adea059fbafd61bb34
Kanchu harish <119840058+kanchuharish24@users.noreply.github.com>
2023-05-19 21:45:02 +0530

    #include<stdio.h> int main() {     int t,d,r,i,n,m,c=0,j;     scanf("%d",&n);     for(r=0;r<n;r++)     {         scanf("%d",&m);         for(i=m;i>2;i--)         {             c=0;             for(j=2;j<i;j++)             {                 if(i%j==0)                 {                     c=1;                     break;                 }             }             if(c==0)             {                 t=i;                 break;             }         }         for(i=m;i<m+100;i++)         {             c=0;             for(j=2;j<i;j++)             {                 if(i%j==0)                 {                     c=1;                     break;                 }             }             if(c==0)             {                 d=i;                 break;             }         }         if((m-t)<=(d-m))         {             printf("%d ",t);         }         else         {             printf("%d ",d);         }     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+{
+    int t,d,r,i,n,m,c=0,j;
+    scanf("%d",&n);
+    for(r=0;r<n;r++)
+    {
+        scanf("%d",&m);
+        for(i=m;i>2;i--)
+        {
+            c=0;
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c==0)
+            {
+                t=i;
+                break;
+            }
+        }
+        for(i=m;i<m+100;i++)
+        {
+            c=0;
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c==0)
+            {
+                d=i;
+                break;
+            }
+        }
+        if((m-t)<=(d-m))
+        {
+            printf("%d
+",t);
+        }
+        else
+        {
+            printf("%d
+",d);
+        }
+    }
+}
