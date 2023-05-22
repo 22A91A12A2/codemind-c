b1940ea9ce5b174de52b943d0e7d166f555a95a7
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2023-05-22 14:58:57 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c,temp;     scanf("%d",&n);     c=a+b;     temp=n;     while(c<=n)     {         c=a+b;         a=b;         b=c;     }     if(n-a>b-n)     {         printf("%d ",b);     }     else if(n-a<b-n)     {         printf("%d ",a);     }     else     {         printf("%d %d",a,b);     }
    }

---
+++ nearest_fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c,temp;
+    scanf("%d",&n);
+    c=a+b;
+    temp=n;
+    while(c<=n)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    if(n-a>b-n)
+    {
+        printf("%d ",b);
+    }
+    else if(n-a<b-n)
+    {
+        printf("%d ",a);
+    }
+    else
+    {
+        printf("%d %d",a,b);
+    }
+    
+}
