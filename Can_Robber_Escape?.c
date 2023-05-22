0fba0c5b4a59dd3e9cabca992d46617173a9015e
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2023-05-22 14:52:22 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             c++;         }     }     if(c<=2)     printf("YES");     else     printf("NO"); }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            c++;
+        }
+    }
+    if(c<=2)
+    printf("YES");
+    else
+    printf("NO");
+}
