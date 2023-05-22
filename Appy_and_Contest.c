4697fbdaaa351d782eb9e6c583827b526db5a0c8
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2023-05-22 14:53:37 +0530

    #include<stdio.h> int main() {     int t;     long long n,a,b,k,count;     scanf("%d",&t);     while(t--)     {         scanf("%lld%lld%lld%lld",&n,&a,&b,&k);         if(a%b==0)         count=n/b-n/a;         else if(b%a==0)         count=n/a-n/b;         else         count=n/a+n/b-2*(n/(a*b));         if(count>=k)         printf("Win ");         else         printf("Lose ");     }
    }

---
+++ Appy_and_Contest.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    long long n,a,b,k,count;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%lld%lld%lld%lld",&n,&a,&b,&k);
+        if(a%b==0)
+        count=n/b-n/a;
+        else if(b%a==0)
+        count=n/a-n/b;
+        else
+        count=n/a+n/b-2*(n/(a*b));
+        if(count>=k)
+        printf("Win
+");
+        else
+        printf("Lose
+");
+    }
+    
+}
